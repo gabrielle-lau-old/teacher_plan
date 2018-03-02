173c31eda1cae4d1d9eac2b7ee849b1fceacaba0
Raghu Betina <raghu@firstdraft.com>
2018-03-02 15:52:58 +0000

    Whitelist columns in Goal for ActiveAdmin

--- app/admin/goals.rb
+++ app/admin/goals.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Goal do
+
+ permit_params :user_id, :action_step, :comments, :improvement_plan_id, :description
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
