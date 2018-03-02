6cb711052367e151d3496ece9149795096ddd545
Raghu Betina <raghu@firstdraft.com>
2018-03-02 15:52:33 +0000

    Direct association between users and collaborations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :collaborations,
+             :dependent => :destroy
+
   has_many   :goals,
              :dependent => :destroy
 
